7572749efab920cde2b091eb7a9808e456d50d72
Denis Artyuhovich <i@denis.by>
2019-02-22 19:05:50 +0200

    Test in safari via browserstack

--- src/components/wdio.test.tsx
+++ src/components/wdio.test.tsx
@@ -28,13 +28,20 @@ afterAll(async () => {
 });
 
 beforeEach(async () => {
+    const capabilities = {
+        'os': 'OS X',
+        'os_version': 'Mojave',
+        'browserName': 'Safari',
+        'browser_version': '12.0',
+        'browserstack.local': 'false',
+        'browserstack.video': 'false',
+    };
+
     browser = await webdriverio.remote({
         user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
         key: process.env.BROWSERSTACK_KEY || 'BROWSERSTACK_KEY',
-        hostname: "hub-cloud.browserstack.com",
-        capabilities: {
-            browserName: process.env.BROWSER_NAME || "chrome",
-        },
+        hostname: "hub.browserstack.com",
+        capabilities: capabilities,
     });
 });
 
@@ -43,21 +50,26 @@ afterEach(async () => {
 });
 
 describe("WDIO", () => {
-    it("Should looks good", async () => {
+    it("Should looks good in safari", async () => {
         await browser.url("about:blank");
         const app = htmlForScreenshot(mount(<App/>).html());
-        await browser.setWindowSize(375, 675);
-        await browser.execute((html: string) => {
-            const HTMLElement = document.querySelector("html");
+
+        // await browser.setWindowSize(375, 675); --- for chrome
+        await browser.setWindowRect(null, null, 375, 675);
+
+        await browser.execute(function (html: string) {
+            var HTMLElement = document.querySelector("html");
             if (HTMLElement) {
                 HTMLElement.innerHTML = html;
             }
         }, app);
 
         const screenShot = await browser.takeScreenshot();
+
         // Needed to convert to correct buffer;
         const str = screenShot.toString('base64');
         const image = Buffer.from(str, 'base64');
+
         expect(image).toMatchImageSnapshot();
     })
 });
