fc403b97ad9de1b5f1a4fec54ae0b74d06eaa473
Denis Artyuhovich <i@denis.by>
2019-03-01 14:17:29 +0200

    Browserstack problem example

--- src/components/wdio.test.tsx
+++ src/components/wdio.test.tsx
@@ -2,42 +2,43 @@ import React from "react";
 import * as webdriverio from "webdriverio";
 import App from "./App";
 import { htmlForScreenshot } from "../utils/testUtils";
-import { mount } from "enzyme";
+import ReactDOMServer from "react-dom/server";
 
 let browser: any;
-
-beforeEach(async () => {
+beforeAll(async () => {
     const capabilities = {
-        'os': 'OS X',
-        'os_version': 'Mojave',
-        'browserName': 'Safari',
-        'browser_version': '12.0',
+        'os_version': '11.3', // will not work, but for 'os_version' : '12', 'device' : 'iPhone XS', everything is fine
+        'device': 'iPhone 8 Plus',
+        'real_mobile': 'true',
         'browserstack.local': 'false',
+        // 'browserstack.appium_version': '1.9.1',
         'browserstack.video': 'false',
-    };
+    } as any;
 
     browser = await webdriverio.remote({
         user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
         key: process.env.BROWSERSTACK_KEY || 'BROWSERSTACK_KEY',
         hostname: "hub.browserstack.com",
         capabilities: capabilities,
+        logLevel: 'error',
     });
 });
 
-afterEach(async () => {
+afterAll(async () => {
     await browser.deleteSession();
 });
 
 describe("WDIO", () => {
     it("Should looks good in safari", async () => {
         await browser.url("about:blank");
-        const app = htmlForScreenshot(mount(<App/>).html());
+        const app = htmlForScreenshot(ReactDOMServer.renderToStaticMarkup(<App/>));
 
-        // await browser.setWindowSize(375, 675); --- for chrome
-        await browser.setWindowRect(null, null, 375, 675);
-
+        // Execute will not work for 'os_version': '11.3', 'device': 'iPhone 8 Plus',
+        // But will work for 'os_version' : '12', 'device' : 'iPhone XS',
+        // Looks like related to: https://github.com/webdriverio/webdriverio/issues/3264
+        // but with 'browserstack.appium_version': '1.9.1' we have issue:
+        // Appium error: An unknown server-side error occurred while processing the command. Original error: Did not get any response after 180s
         await browser.execute(function (html: string) {
-
             // es5! as will be executed in browser context
             var HTMLElement = document.querySelector("html");
             if (HTMLElement) {
